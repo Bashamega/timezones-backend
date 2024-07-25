eb276133649f6743f4021a93fb0d8b54b097450b
Bashamega <adambashaahmednaji@gmail.com>
2024-07-25 13:06:07 +0300

    Endpoints

--- src/timezones/timezones.controller.ts
+++ src/timezones/timezones.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body } from '@nestjs/common';
 
 @Controller('timezones')
-export class TimezonesController {}
+export class TimezonesController {
+    @Get()
+    home() {
+        return "Nothing to see here"
+    }
+
+    @Get(':userId')
+    findTimeZone(@Param('userId') dcUserId: string) {
+        return(
+            {
+                userId: dcUserId,
+                timeZone: ""
+            }
+        )
+    }
+
+    @Post(':userId')
+    registerTimezone(@Param('userId') dcUserId: number, @Body() body:{timezone:string}){
+        return({
+            userId:dcUserId,
+            message: "Added",
+            tiemzone: body.timezone
+        })
+    }
+}
