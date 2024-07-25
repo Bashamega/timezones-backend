013c25df053e23c2ea80e4ad6723d8e4a1866717
Bashamega <adambashaahmednaji@gmail.com>
2024-07-25 14:33:03 +0300

    Typeorm Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TimezonesController } from './timezones/timezones.controller';
 import { TimezonesService } from './timezones/timezones.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(
+    {
+      type:'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true
+    }
+  )],
   controllers: [AppController, TimezonesController],
   providers: [AppService, TimezonesService],
 })
