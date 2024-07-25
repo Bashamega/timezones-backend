0f46cc2eb0d4df7a58dacf60c05f0b07c596047c
Bashamega <adambashaahmednaji@gmail.com>
2024-07-25 14:18:58 +0300

    New service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TimezonesController } from './timezones/timezones.controller';
+import { TimezonesService } from './timezones/timezones.service';
 
 @Module({
   imports: [],
   controllers: [AppController, TimezonesController],
-  providers: [AppService],
+  providers: [AppService, TimezonesService],
 })
 export class AppModule {}
